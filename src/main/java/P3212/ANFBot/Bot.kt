cfa8626ee3fe6f640f92261e1621b2c352b46203
pr0p1k <propiktunes@gmail.com>
2018-11-13 01:58:27 +0300

    kinda works. Responds with caps

--- src/main/java/P3212/ANFBot/Bot.kt
+++ src/main/java/P3212/ANFBot/Bot.kt
@@ -9,20 +9,29 @@ import java.lang.Exception
 import java.lang.IllegalStateException
 import javax.annotation.Resource
 import javax.jms.*
+import javax.jms.ConnectionFactory
 
 class Bot : TelegramLongPollingBot() {
     private val token = "754140988:AAHSkm4XIO8TXMxvlU49JHJJtYPQprNlUJM"
     private val queueName = "bot"
+    private val factory = RMQConnectionFactory()
+    private val con = factory.createConnection()
+    private val session = con.createSession(false, Session.AUTO_ACKNOWLEDGE)
+    private val producer = session.createProducer(session.createQueue("bot"))
+    private val consumer = session.createConsumer(session.createQueue("botResponse"))
     override fun getBotToken(): String {
         return token
     }
 
+    init {
+        factory.host = "localhost"
+    }
 
     override fun onUpdateReceived(update: Update) {
         if (update.hasMessage() && update.message.hasText()) {
             val id = update.message.chatId;
-            val list = update.message.text.toLowerCase().split(" ")
-            when (list[0]) {
+            val list = update.message.text.split(" ")
+            when (list[0].toLowerCase()) {
                 "/help", "help" -> answer("Available commands are:\n", id)
                 "/top", "top" -> request("top", id)
                 "/events", "events" -> request("events", id)
@@ -36,20 +45,11 @@ class Bot : TelegramLongPollingBot() {
     }
 
     private fun request(msg: String, chatId: Long) {
-        val factory = RMQConnectionFactory()
-        factory.host = "localhost"
-        val con = factory.createConnection()
-        val session = con.createSession(false, Session.AUTO_ACKNOWLEDGE)
-        val producer = session.createProducer(session.createQueue("bot"))
         con.start()
         producer.send(session.createTextMessage(msg))
-        val consumer = session.createConsumer(session.createQueue("botResponse"))
         val message = consumer.receive(3000)
-        answer((message as TextMessage).text, chatId)
-    }
-
-    private fun getResponse() {
-
+        answer((message as? TextMessage)?.text ?: "An internal error occurred", chatId)
+        con.stop()
     }
 
 
